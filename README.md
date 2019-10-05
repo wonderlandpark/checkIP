a9bb0424be9d0e117bba2b257d764556710a0cb6
JUN SEO PARK <wonderbotsupport@naver.com>
2019-10-06 01:54:25 +0900

    EveryThing

---
+++ README.md
@@ -0,0 +1,23 @@
+# To Use This...
+
+## Install Required Packages
+### Backend
+```bash
+cd backend && npm i
+```
+### Frontend
+```bash
+cd fronted && npm i 
+```
+
+## Run
+```bash
+cd backend && npm start
+```
+Open another Terminal
+```bash
+cd fronted && npm run dev
+```
+
+## Enjoy
+It will be running at 8080 port!
---
+++ frontend/src/components/index.vue
@@ -0,0 +1,25 @@
+/* eslint-disable */
+<template> 
+<div class="movies"> 
+    <h1>내 아이피</h1> 
+    <p>{{info.data.ip}}</p>
+       </div>
+
+       </template>
+
+    
+<script>
+export default {
+created(){ 
+var id = this.$route.params.id; 
+this.$http.get(`/json`).then(response => { this.info = response; console.log(response)}); },
+data(){
+    return {
+        info : {}
+    }
+},
+
+   
+}
+</script>
+
